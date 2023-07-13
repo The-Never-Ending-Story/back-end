d783bcee0ab8ed52a7eea7dbb47d5a8ad071fe2b
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-07-13 00:37:46 -0500

    Fix: remove fields from worlds serializer

--- worlds/serializers.py
+++ worlds/serializers.py
@@ -4,5 +4,5 @@ from .models import World
 class WorldSerializer(serializers.ModelSerializer):
     class Meta:
           model = World
-          fields = ['id', 'name', 'blurb', 'description', 'species', 'geodynamics', 'magictechnology', 'img', 'characters', 'locations', 'events', 'history' ]
+          fields = ['id', 'name', 'blurb', 'description', 'species', 'geodynamics', 'magictechnology', 'img', 'history']
 
