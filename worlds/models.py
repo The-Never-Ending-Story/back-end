57a3c155fab93c09a07448568d4070888160f53b
Andrew Bowman <andrewtbowman@gmail.com>
2023-07-13 10:16:38 -0400

    remove events,locations,chars from world model, add to serializer, camelCase attributes

--- worlds/models.py
+++ worlds/models.py
@@ -1,18 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class World(models.Model):
     name = models.CharField(max_length=250)
     blurb = models.CharField(max_length=250)
     description = models.TextField()
     discovered = models.BooleanField(default=False)
-    species = models.JSONField()
-    geodynamics = models.JSONField()
-    magictechnology = models.JSONField()
-    img = models.TextField(default='none')
-    characters = models.JSONField(default='none')
-    locations = models.JSONField(default='none')
-    events = models.JSONField(default='none')
-    history = models.TextField(default='none')
+    species = ArrayField(models.JSONField())
+    geoDynamics = models.JSONField()
+    magicTechnology = models.JSONField()
+    img = ArrayField(models.TextField(default='none'), blank=True)
+    history = ArrayField(models.TextField(default='none'))
 
     def __str__(self):
         return str(self.id) + " " + self.name
--- worlds/serializers.py
+++ worlds/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
 from .models import World
 
-class WorldSerializer(serializers.ModelSerializer):
-    class Meta:
-          model = World
-          fields = ['id', 'name', 'blurb', 'description', 'discovered', 'species', 'geodynamics', 'magictechnology', 'img', 'characters', 'locations', 'events', 'history' ]
+from .characters.models import Character
+from .characters.serializers import CharacterSerializer
+from .locations.models import Location
+from .locations.serializers import LocationSerializer
+from .events.models import Event
+from .events.serializers import EventSerializer
 
+class WorldSerializer(serializers.ModelSerializer):
+    characters = serializers.SerializerMethodField()
+    locations = serializers.SerializerMethodField()
+    events = serializers.SerializerMethodField()
+
+    class Meta:
+        model = World
+        fields = ['id', 'name', 'blurb', 'description', 'discovered', 'species', 'geodynamics', 'magictechnology', 'img', 'characters', 'locations', 'events', 'history' ]
+
+    def get_characters(self, obj):
+        characters = Character.objects.filter(world=obj)
+        serializer = CharacterSerializer(characters, many=True)
+        return serializer.data
+
+    def get_locations(self, obj):
+        locations = Location.objects.filter(world=obj)
+        serializer = LocationSerializer(locations, many=True)
+        return serializer.data
+
+    def get_events(self, obj):
+        events = Event.objects.filter(world=obj)
+        serializer = EventSerializer(events, many=True)
+        return serializer.data
