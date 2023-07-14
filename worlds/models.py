d8cebe68169f0cb05e2040a5902846b41748d88b
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-07-13 22:56:13 -0500

    Fix: re-add incoming change that somehow got removed in merge

--- worlds/models.py
+++ worlds/models.py
@@ -9,7 +9,19 @@ class World(models.Model):
     geoDynamics = models.JSONField(default=dict)
     magicTechnology = models.JSONField(default=dict)
     img = models.TextField(default='none')
-    history = models.TextField(default=dict)
+    history = models.TextField(default='none')
+
+    @property
+    def locations(self):
+        return self.location_set.all()
+
+    @property
+    def characters(self):
+        return self.character_set.all()
+
+    @property
+    def events(self):
+        return self.event_set.all()
 
     def __str__(self):
         return str(self.id) + " " + self.name
