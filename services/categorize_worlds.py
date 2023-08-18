3c7fe5ab23d9a710f9b485320e810344fc6d34af
Andrew Bowman <andrewtbowman@gmail.com>
2023-08-18 15:21:11 -0400

    add django defaults to script

--- services/categorize_worlds.py
+++ services/categorize_worlds.py
@@ -1,3 +1,9 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")  # Update with your actual settings module
+
+import django
+django.setup()
+
 from .attributes import categorize_world, CATEGORIES
 from worlds.models import World
 
