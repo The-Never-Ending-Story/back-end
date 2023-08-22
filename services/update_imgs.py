9501d3342870d4678024b6cc488289338ca4b5f2
Andrew Bowman <andrewtbowman@gmail.com>
2023-08-22 10:17:25 -0400

    add check for valid image urls

--- services/update_imgs.py
+++ services/update_imgs.py
@@ -7,21 +7,30 @@ import re
 
 def compress_thumbnails():
   worlds = World.objects.all()  
+  pattern = re.compile(r"\.png$")
 
   for world in worlds:
-      pattern = re.compile(r"\.png$")
+      print(f"working on world {world.id}")
       thumbnail_url = world.img.get("thumbnail")
       if thumbnail_url and pattern.search(thumbnail_url):
-        response = requests.get(thumbnail_url)
-        img_data = BytesIO(response.content)
-        img = Image.open(img_data)
-        img.thumbnail((200, 200))
+          response = requests.get(thumbnail_url)
+          
+          if response.status_code == 200:
+              print("image fetched")
+              img_data = response.content
+              img = Image.open(BytesIO(img_data))
+              img.thumbnail((200, 200))
 
-        compressed_img_data = BytesIO()
-        img.save(compressed_img_data, format='PNG', quality=95)  
+              compressed_img_data = BytesIO()
+              img.save(compressed_img_data, format='PNG', quality=95)
 
-        world.thumbnail = compressed_img_data.getvalue()
-        world.save()
+
+              world.thumbnail = compressed_img_data.getvalue()
+              print("image compressed")
+              world.save()
+          else:
+              print(f"Failed to fetch image: {thumbnail_url}")
+
 
 compress_thumbnails()
 # update_all_images()
