780f31e5bd8bbd73a783df8ec483e4230ab6a6b4
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-08-24 13:28:18 -0500

    Fix: update tests with the latest API changes

--- worlds/tests/characters/test_get_world_character.py
+++ worlds/tests/characters/test_get_world_character.py
@@ -1,57 +1,14 @@
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
-
-from worlds.models import World
-from locations.models import Location
-from characters.models import Character
-
-
-@pytest.fixture
-def mock_world():
-    return World.objects.create(
-        name='Magic World',
-        blurb='A magical world',
-        description='A world of high fantasy and powerful magics',
-        discovered=False,
-        geoDynamics={'origin': 'mountains'},
-        magicTechnology={'origin': 'ancient'},
-        img={'thumbnail': 'https://imgur.com/gallery/world123'}
-    )
-
-
-@pytest.fixture
-def mock_location(mock_world):
-    return Location.objects.create(
-        name='Magic City',
-        type='city',
-        climate='rainy',
-        lore='An ancient city of wonder',
-        imagine='Imagine a city of ancient magics',
-        img='https://imgur.com/gallery/location123',
-        world=mock_world
-    )
-
-
-@pytest.fixture
-def mock_character(mock_world, mock_location):
-    return Character.objects.create(
-        name='Joe Bob',
-        species='human',
-        alignment='lawful good',
-        age=35,
-        lore='A valiant warrior',
-        imagine='Imagine a strong warrior',
-        img='https://imgur.com/gallery/character123',
-        location='The forest',
-        world=mock_world,
-    )
+from fixtures.characters import mock_characters
+from fixtures.worlds import mock_worlds
 
 
 @pytest.mark.django_db
-def test_get_character_valid(mock_character, mock_world):
+def test_get_character_valid(mock_characters, mock_worlds):
     client = APIClient()
-    url_ids = {'world_id': mock_world.id, 'id': mock_character.id}
+    url_ids = {'world_id': mock_worlds[0].id, 'id': mock_characters[0].id}
     url = reverse('get_world_character', kwargs=url_ids)
     response = client.get(url)
 
@@ -68,6 +25,7 @@ def test_get_character_valid(mock_character, mock_world):
     assert 'lore' in character
     assert 'imagine' in character
     assert 'img' in character
+    assert 'imgs' in character
     assert 'location' in character
     assert 'world' in character
 
@@ -79,14 +37,15 @@ def test_get_character_valid(mock_character, mock_world):
     assert type(character['lore']) is str
     assert type(character['imagine']) is str
     assert type(character['img']) is str
+    assert type(character['imgs']) is list
     assert type(character['location']) is str
     assert type(character['world']) is int
 
 
 @pytest.mark.django_db
-def test_get_world_character_invalid_world(mock_character):
+def test_get_world_character_invalid_world(mock_characters):
     client = APIClient()
-    url_ids = {'world_id': 5678, 'id': mock_character.id}
+    url_ids = {'world_id': 5678, 'id': mock_characters[0].id}
     url = reverse('get_world_location', kwargs=url_ids)
     response = client.get(url)
 
@@ -94,9 +53,9 @@ def test_get_world_character_invalid_world(mock_character):
 
 
 @pytest.mark.django_db
-def test_get_world_character_invalid_character(mock_world):
+def test_get_world_character_invalid_character(mock_worlds):
     client = APIClient()
-    url_ids = {'world_id': mock_world.id, 'id': 5678}
+    url_ids = {'world_id': mock_worlds[0].id, 'id': 5678}
     url = reverse('get_world_location', kwargs=url_ids)
     response = client.get(url)
 
